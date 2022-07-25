ab4f4485fa7a2a86d321b9900b475770861a3ae6
DeJesusMartinezArturo <al222110852@gmail.com>
2022-07-25 18:37:38 -0500

    cambios en servicios

--- components/servicios.php
+++ components/servicios.php
@@ -2,12 +2,17 @@
 <html lang="es">
 
 <head>
-  <title>Servicios</title>
-  <link href="./assets/css/s.css" type="text/css" rel="stylesheet" media="screen,projection" />
-  <meta charset="utf-8">
-  <link href="../assets/css/styles.css" type="text/css" rel="stylesheet" media="screen,projection" />
-  <link href="../assets/css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection" />
-  <link href="../assets/css/styles.css" type="text/css" rel="stylesheet" media="screen,projection" />
+<title>Servicios</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="./assets/css/s.css" type="text/css" rel="stylesheet" media="screen,projection" />
+    <meta charset="utf-8">
+    <link href="../assets/css/styles.css" type="text/css" rel="stylesheet" media="screen,projection" />
+    <link href="../assets/css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection" />
+    <link href="../assets/css/styles.css" type="text/css" rel="stylesheet" media="screen,projection" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
 </head>
 <?php
 //Includes es el directorio donde estaría header.php. Puedes darle el nombre que quieras
@@ -15,66 +20,163 @@
     ?>
 
 <body class="seccion">
+<div class="seccion">
+        <h2>Medico</h2>
+        <h5>Servicios</h5>
+    </div>
 
-  <div class="contenedor-citas">
-    <h1>Reservación</h1>
-    <p >Elige tus servicios a continuación</p>
+
+    <div class="row">
+        <div class="col-sm-4">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title ">Desparasitacion</h5>
+                    <p class="card-text">COSTO: $ pesos</p>
+                    <a href="#" class="btn btn-primary">Reservar</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-4">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Consulta Medica</h5>
+                    <p class="card-text">COSTO: $ pesos</p>
+                    <a href="#" class="btn btn-primary">Reservar</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-4">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Vacunas</h5>
+                    <p class="card-text">COSTO: $ pesos</p>
+                    <a href="#" class="btn btn-primary">Reservar</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<div class="servicios">
+    <div class="card text-center col-sm-12">
+  <div class="card-header">
+  Nuevo
   </div>
+  <img class="card-img-top" src="/assets/img/promo1.png" alt="Card image cap">
+  <div class="card-body ">
+    <p class="card-text">Croquetas para perros adultos de razas grandes, con ingredientes seleccionados para aportar una alimentación completa y equilibrada para minimizar los efectos del envejecimiento.
+</p>
+    <a href="#" class="btn btn-primary">Ver</a>
+  </div>
+  <div class="card-footer text-muted">
+    4 days ago
+  </div>
+</div>
+<div class="card text-center col-sm-12">
+  <div class="card-header">
+  Nuevo
+  </div>
+  <img class="card-img-top" src="/assets/img/promo3.png" alt="Card image cap">
+  <div class="card-body ">
+    <p class="card-text">Nuevas bolsas de puerta limpias + 360 bolsas higiénicas para caca de perros</p>
+    <a href="#" class="btn btn-primary">Ver</a>
+  </div>
+  <div class="card-footer text-muted">
+    7 days ago
+  </div>
+</div>
+<div class="card text-center col-sm-12">
+  <div class="card-header bg-warning" >
+  ¡Super Precio!
+  </div>
+  <img class="card-img-top" src="/assets/img/promo2.png" alt="Card image cap">
+  <div class="card-body ">
+    <p class="card-text">El transportín Robustus de Outech es un artículo con doble cerradura para llevar con total seguridad a tu mascota en el coche. </p>
+    <a href="#" class="btn btn-primary">Reservar</a>
+  </div>
+  <div class="card-footer text-muted">
+    2 days ago
+  </div>
+</div>
+</div>
 
-  <div>
-    <nav class="tabs">
-      <button type="button" data-paso="1">Servicios</button>
-      <button type="button" data-paso="2">Información Cita</button>
-      <button type="button" data-paso="3">Resumen</button>
-    </nav>
 
-    <h5>Seleccione uno de los servicios a continuación...</h5>
-    <h2>Medico</h2>
-    <h5>Servicios</h5>
-
-    <div class="servicios">
-      <section class="contenedor_servicios">
-        <h3>Desparasitacion</h3>
-        <p>COSTO: $ pesos</p>
-      </section>
-
-      <section class="contenedor_servicios">
-        <h3>Consulta Medica</h3>
-        <p>COSTO: $ pesos</p>
-      </section>
-
-      <section class="contenedor_servicios">
-        <h3>Vacunas</h3>
-        <p>COSTO: $ pesos</p>
-      </section>
-    </div>
-    <h2>Estética</h2>
-    <h5>Paquetes</h5>
-    <div class="servicios">
-      <section class="contenedor_servicios">
-        <h3>Fancy</h3>
-        <p>baño</p>
-        <p>COSTO: $ pesos</p>
-      </section>
-
-      <section class="contenedor_servicios">
-        <h3>Príncipe Princesa</h3>
-        <p>- Baño</p>
-        <p>COSTO: $ pesos</p>
-      </section>
-
-      <section class="contenedor_servicios">
-        <h3>Rey</h3>
-        <p>- Baño con masaje
-        </p>
-        <p>COSTO: $ pesos</p>
-      </section>
+    <div class="seccion">
+        <h2>Estética</h2>
+        <h5>Paquetes</h5>
     </div>
 
-    <div class="navegacion">
-      <button><a href="../components/login.php">Volver &raquo; </a></button>
-      <button><a href="../components/informacion.php">Siguiente &raquo; </a></button>
+    <div class="row">
+        <div class="col-sm-4">
+            <div class="card">
+                <h5 class="card-title ">Fancy</h5>
+
+                <div class="card-body">
+                    <p class="card-text">Baño</li>
+                    <p class="card-text">Secado rápido</li>
+                    <p class="card-text">Corte de uñas</li>
+                    <p class="card-text">COSTO: $ pesos</p>
+
+                    <a href="#" class="btn btn-primary ">Reservar</a>
+
+                    </div>
+            </div>
+
+        </div>
+
+        <div class="col-sm-4">
+            <div class="card">
+                <h5 class="card-title ">Príncipe Princesa</h5>
+
+                <div class="card-body">
+                    <p class="card-text">Baño</li>
+                    <p class="card-text">Secado rápido</li>
+                    <p class="card-text">Corte de uñas</li>
+                    <p class="card-text">Corte de pelo</li>
+                    <p class="card-text">Arreglo estético</li>
+                    <p class="card-text">COSTO: $ pesos</p>
+
+                    <a href="#" class="btn btn-primary">Reservar</a>
+                    </div>
+
+            </div>
+
+        </div>
+        <div class="col-sm-4">
+            <div class="card">
+                <h5 class="card-title  ">Rey</h5>
+
+                <div class="card-body">
+                    <p class="card-text">Baño</li>
+                    <p class="card-text">Secado rápido</li>
+                    <p class="card-text">Corte de uñas</li>
+                    <p class="card-text">Corte de pelo</li>
+                    <p class="card-text">Arreglo estético</li>
+                    <p class="card-text">Limpieza de oídos</li>
+                    <p class="card-text">Lavado de dientes</li>
+                    <p class="card-text">Adorno y perfume</li>
+                    <p class="card-text">COSTO: $ pesos</p>
+
+                    <a href="#" class="btn btn-primary">Reservar</a>
+                </div>
+
+
+            </div>
+        </div>
+
     </div>
+
+    
+    <div class="col-sm-12 ">
+      <a href="../components/login.php" class="btn btn-primary ">Volver &raquo; </a>
+      <a href="../components/informacion.php" class="btn btn-primary">Siguiente &raquo; </a>
+    </div>
+
+    </div>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
   </div>
 
